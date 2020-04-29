f72dcb4dbc25761f13f61bdb5dadb17a1d2268ab
srgssj <ssjsrgx@gmail.com>
2020-04-29 14:08:33 +0200

    cookies

--- js/main.js
+++ js/main.js
@@ -62,6 +62,15 @@
 	// Scrollax
    $.Scrollax();
 
+   $(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+}); 
+
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
 	    loop:true,
